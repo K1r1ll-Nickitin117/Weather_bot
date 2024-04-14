8633778e5b43571628e5b24ce8578f9e4a2336e1
PC <n17k17@yandex.ru>
2024-04-14 16:34:51 +0300

    ////

--- bot.py
+++ bot.py
@@ -55,6 +55,26 @@ class Bot:
     async def process_what_to_wear_command(message: Message):
         await message.answer(what_to_wear_phrase)
 
+    @dp.message(Command(commands=['location_now']))
+    async def location_now_command(message: Message):
+        await message.answer(location_now_phrase)
+
+    @dp.message(Command(commands=['weather']))
+    async def weather_command(message: Message):
+        await message.answer(weather_phrase)
+
+    @dp.message(Command(commands=['my_location']))
+    async def my_location_command(message: Message):
+        await message.answer(my_location_phrase)
+
+    @dp.message(Command(commands=['change_location']))
+    async def change_location_command(message: Message):
+        await message.answer(change_location_phrase)
+
+    @dp.message(Command(commands=['what_to_wear']))
+    async def what_to_wear_command(message: Message):
+        await message.answer(what_to_wear_phrase)
+
     @dp.message()
     async def send_echo(message: Message):
         await message.reply(if_the_message_has_not_been_processed_phrase)
