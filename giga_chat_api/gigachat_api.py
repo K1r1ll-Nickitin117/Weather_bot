eb547731c49e4ce0d2913e77cafe996a9f954ce0
K1R1LL117 <n17k17@yamdex.ru>
2024-04-21 20:32:25 +0300

    ////52...

--- giga_chat_api/gigachat_api.py
+++ giga_chat_api/gigachat_api.py
@@ -1,9 +1,6 @@
 from langchain.schema import HumanMessage, SystemMessage
 from langchain.chat_models.gigachat import GigaChat
 from settings.config import AUTHORIATION_DATA
-import requests
-import json
-from pprint import pprint
 
 
 def giga_chat_weather(weather_info):
